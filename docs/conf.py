1d482c5501541f380933242bec7d8767cf96f6ab
charlesxwang <charles.cfw@gmail.com>
2020-01-07 02:03:03 +0000

    delete

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ rst_prolog = """
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinxcontrib.bibtex', 'sphinxtogithub'
+	'sphinxcontrib.bibtex'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
